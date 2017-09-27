e4fd90f805aaaa06178c58aa4eb3ef88a6fb929b
rodrigo <luis.mendoza@grupoporvenir.com.mx>
2017-09-27 10:28:59 -0500

    login para traer tercer nivel

--- menus.sql
+++ menus.sql
@@ -10,7 +10,6 @@ FROM CATALOGOS.dbo.tc_aplicaciones
     VALUES('SFP', 'http://192.168.1.14/app', 26, 1, 'fa fa-times')
 
 */
-
 SELECT *
 FROM CATALOGOS.dbo.tc_menus
 
@@ -34,3 +33,28 @@ FROM CATALOGOS.dbo.tc_menus
     VALUES(25, 'Revisión', 269, '#/fraudPrevention/review', 1, 2, '{ active: view.page == "/fraudPrevention/review" }', '')
 
 */
+
+INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+VALUES(25, 'SIGAQ', 0, '#', 1, 1, '{active:view.page == "/brokenessManagement/audit" || view.page == "/brokenessManagement/responsable/" + view.urlEmployee || view.page == "/brokenessManagement/humanTalent" || view.page == "/brokenessManagement/finance" || view.page == "/brokenessManagement/brokenessPayment"}', 'fa fa-chain-broken')
+
+INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+VALUES(25, 'Auditoria', 271, '#/brokenessManagement/audit', 1, 2, '{active:view.page == "/brokenessManagement/audit" || view.page == "/brokenessManagement/responsable/" + view.urlEmployee }', '')
+
+INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+VALUES(25, 'Talento Humano', 271, '#/brokenessManagement/humanTalent', 1, 3, '{active:view.page == "/brokenessManagement/humanTalent"}', '')
+
+INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+VALUES(25, 'Finanzas', 271, '#', 1, 4, '{active:view.page == "/brokenessManagement/finance" || view.page == "/brokenessManagement/brokenessPayment"}', '')
+
+INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+VALUES(25, 'Asignación', 274, '#/brokenessManagement/finance', 1, 5, '', '')
+
+INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+VALUES(25, 'Cobro', 274, '#/brokenessManagement/brokenessPayment', 1, 6, '', '')
+
+UPDATE CATALOGOS.dbo.tc_menus SET
+	--tcmenu_descrip = 'Asignación'
+	--tcmenu_url = '#'
+	tcmenu_class = '{active:view.page == "/brokenessManagement/audit" || view.page == "/brokenessManagement/responsable/" + view.urlEmployee || view.page == "/brokenessManagement/humanTalent" || view.page == "/brokenessManagement/finance" || view.page == "/brokenessManagement/brokenessPayment"}'
+where tcmenu_id = 271
+	/*or tcmenu_id = 276*/
