0609966d60ade342f01c9c17eae94ca21e6ed96f
rodrigoMendoza <luis.mendoza@grupoporvenir.com.mx>
2018-04-18 11:18:29 -0500

    va

--- menusSFP.sql
+++ menusSFP.sql
@@ -16,7 +16,7 @@ UPDATE CATALOGOS.dbo.tc_aplicaciones SET [icon_ap] = 'fa fa-times'
 
 --inserta aplicacion
 INSERT INTO CATALOGOS.dbo.tc_aplicaciones (nombre_ap, url_ap, orden, estatus, icon_ap)
-VALUES('SFP', 'http://192.168.1.6/app', 25, 1, 'fa fa-times')
+VALUES('SFP', 'http://192.168.1.4/app', 22, 1, 'fa fa-times')
 
 --inserta menus de aplicacion
 
@@ -51,27 +51,29 @@ BEGIN
     VALUES(22, 'Reportes', 230, '#', 1, 3, '{ active: view.page == "/collection/report/condonation" || view.page == "/collection/report/management" }', '')
 
 	INSERT INTO CATALOGOS.dbo.tc_menus (tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
-	VALUES(22,'Condonaciones', 235, '#/collection/report/condonation', 1, 6, '{ active:view.page == "/collection/report/condonation" }', '')
-	, (22,'Gestión', 235, '#/collection/report/management', 1, 7, '{ active:view.page == "/collection/report/management" }', '')
+	VALUES(22,'Condonaciones', 232, '#/collection/report/condonation', 1, 4, '{ active:view.page == "/collection/report/condonation" }', '')
+	, (22,'Gestión', 232, '#/collection/report/management', 1, 5, '{ active:view.page == "/collection/report/management" }', '')
 
 
 END
 
 -- PLD
-BEGIN
-	INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
-	VALUES(22, 'P. Lavado de dinero', 0,'#', 1, 1, '{ active: view.page == "/pld/dailyRequirement" || view.page == "/pld/authconfig" || view.page == "/pld/inquiries" || view.page == "/pld/wisw" }', 'fa fa-shield')
+--BEGIN
+--	INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+--	VALUES(22, 'P. Lavado de dinero', 0,'#', 1, 1, '{ active: view.page == "/pld/dailyRequirement" || view.page == "/pld/authconfig" || view.page == "/pld/inquiries" || view.page == "/pld/wisw" }', 'fa fa-shield')
 
-	INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
-	VALUES(22, 'Requerimiento diario', 232, '#/pld/dailyRequirement', 1, 2, '{ active: view.page == "/pld/dailyRequirement" }', '')
+--	INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+--	VALUES(22, 'Requerimiento diario', 232, '#/pld/dailyRequirement', 1, 2, '{ active: view.page == "/pld/dailyRequirement" }', '')
 
-	INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
-    VALUES(22, 'Autoridades', 232, '#/pld/authconfig', 1, 3, '{ active: view.page == "/pld/authconfig" }', '')
+--	INSERT INTO CATALOGOS.dbo.tc_menus(tcmenu_id_ap, tcmenu_descrip, tcmenu_parent, tcmenu_url, tcmenu_estatus, tcmenu_order, tcmenu_class, tcmenu_icon)
+--    VALUES(22, 'Autoridades', 232, '#/pld/authconfig', 1, 3, '{ active: view.page == "/pld/authconfig" }', '')
 
-END
+--END
 
 SELECT *
 FROM CATALOGOS.dbo.tc_menus
+WHERE tcmenu_id > 200
 
 SELECT *
 FROM CATALOGOS.dbo.tc_aplicaciones
+ORDER BY orden ASC
